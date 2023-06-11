5b12bef60b701f70250515202bcf3ad5a5916519
behshad <behshadrad04@gmail.com>
2023-06-11 10:34:39 +0330

    che shode

--- src/page/About/index.jsx
+++ src/page/About/index.jsx
@@ -3,6 +3,7 @@ import { useTranslation } from "react-i18next";
 
 import { BiWorld } from "react-icons/bi";
 
+
 const About = () => {
   const { t, i18n } = useTranslation();
 
@@ -10,7 +11,6 @@ const About = () => {
     <main className="bg-slate-300 container mx-auto mt-20 flex flex-col items-center  gap-2 ">
       <h1>Multi-language app</h1>
       <BiWorld size={44} />
-
       <h1>{t("Home")}</h1>
     </main>
   );
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,12 +2,15 @@ import App from "../App";
 import About from "../page/About";
 import Home from "../page/Home";
 import Contact from "../page/contact";
+import ThemeToggle from "../components/toggle";
 
 let routes = [
   { path: "/", element: <Home /> },
   { path: "/about", element: <About /> },
   { path: "/Contact", element: <Contact /> },
+  { path: "/ThemeToggle", element: <ThemeToggle /> },
 ];
 
 export default routes;
 
+;
