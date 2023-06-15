144a9ca10d7f84d7003b519134660fa4b8531cc7
behshad <behshadrad04@gmail.com>
2023-06-15 23:08:34 +0330

    add sildder contact

--- src/page/contact/index.jsx
+++ src/page/contact/index.jsx
@@ -1,68 +1,59 @@
-// import { useTranslation } from "react-i18next";
+import { useTranslation } from "react-i18next";
 
-// const Contact = () => {
-//   const { t, i18n } = useTranslation();
-//   return (
-//     <div className="bg-slate-300 container mx-auto mt-20 flex flex-col items-center  gap-2" >
-//       <h1>{t("Contact")}</h1>
-//     </div>
-//   );
-// };
-
-// export default Contact;
-// ????????????????????????????
-import React, { useCallback } from "react";
-
-
-import {
-  ReactCompareSlider,
-  ReactCompareSliderHandle,
-  ReactCompareSliderImage,
-} from "react-compare-slider";
-
-
-import a from "../../static/40718.jpg"
-import b from "../../static/mostangjpg.jpeg"
-import Slide from "../../components/navbar/ SlideableMenuSwiper";
-/** With Customised `handle`. */
-const Contact = ({ style, ...props }) => {
-  const handlePositionChange = useCallback(
-    (position) => console.log("[Portrait]", position),
-    []
-  );
-
+import "./contact.css"
+const Contact = () => {
+  const { t, i18n } = useTranslation();
   return (
-    <div>
-      <ReactCompareSlider
-        className=""
-        {...props}
-        handle={
-          <ReactCompareSliderHandle
-            buttonStyle={{
-              backdropFilter: undefined,
-              border: 0,
-              color: "#333",
-            }}
-          />
-        }
-        itemOne={
-          <ReactCompareSliderImage
-            src={a}
-            style={{ filter: "grayscale(1)" }}
-            alt="one"
-          />
-        }
-        itemTwo={<ReactCompareSliderImage src={a} alt="two" />}
-        onPositionChange={handlePositionChange}
-        style={{
-          display: "flex",
-          width: "100%",
-          height: "50vh",
-          ...style,
-        }}
-      />
-      <h1>slide</h1>
-      <Slide/>
+    <div className="bg-slate-300 container mx-auto mt-20 flex flex-col items-center  gap-2">
+      <h1>{t("Contact")}</h1>
+      <div class="container">
+        <div class="item">
+          <div class="quote">
+            <p>
+              I am Groot.<span>Groot</span>
+            </p>
+          </div>
+        </div>
+        <div class="item">
+          <div class="quote">
+            <p>
+              You'r the head of security and your password is 'password'&nbsp;?
+              <span>Peter Parker</span>
+            </p>
+          </div>
+        </div>
+        <div class="item">
+          <div class="quote">
+            <p>
+              That really is America's ass.<span>Captain America</span>
+            </p>
+          </div>
+        </div>
+        <div class="item">
+          <div class="quote">
+            <p>
+              The hardest choices require the strongest wills.
+              <span>Thanos</span>
+            </p>
+          </div>
+        </div>
+        <div class="item">
+          <div class="quote">
+            <p>
+              Genius, billionaire, playboy, philanthropist.
+              <span>Tony Stark</span>
+            </p>
+          </div>
+        </div>
+        <div class="item">
+          <div class="quote">
+            <p>
+              We never lose our demons. We only learn to live above them.
+              <span>Ancient One</span>
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
